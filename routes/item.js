4cce99b3291fac6e8cef62da4f6f09d7de23eb90
Matheus Filipe T. Chaves <matheus.02468@gmail.com>
2017-06-16 07:33:48 -0300

    Remove token validation on product and item routes.

--- routes/item.js
+++ routes/item.js
@@ -1,10 +1,9 @@
 module.exports = function (app) {
     let controller = app.controllers.item;
-    let validate = app.controllers.authentication.validate;
 
     app.route("/items")
-        .get(validate, controller.findAll);
+        .get(controller.findAll);
 
     app.route("/items/:_id")
-        .get(validate, controller.findById);
+        .get(controller.findById);
 };
--- routes/product.js
+++ routes/product.js
@@ -1,10 +1,9 @@
 module.exports = function (app) {
     let controller = app.controllers.product;
-    let validate = app.controllers.authentication.validate;
 
     app.route("/products")
-        .get(validate, controller.findAll);
+        .get(controller.findAll);
 
     app.route("/products/:_id")
-        .get(validate, controller.findById);
+        .get(controller.findById);
 };
