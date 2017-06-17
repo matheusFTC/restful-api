88c940101ebe5d84cffb8c05ac665870ddaa8b96
Matheus Filipe Teixeira Chaves <matheus.02468@gmail.com>
2017-06-17 10:12:42 -0300

    Adjusted the mock controll.

--- controllers/mock.js
+++ controllers/mock.js
@@ -8,82 +8,94 @@ module.exports = function (app) {
 
         Product.remove({}).then(function () {
             console.log("Remove all products.");
-        });
 
-        Item.remove({}).then(function () {
-            console.log("Remove all items.");
-        });
+            Item.remove({}).then(function () {
+                console.log("Remove all items.");
 
-        let bread = new Item({
-            name: "Bread",
-            description: "Bread toast.",
-            price: 0.00
-        });
+                let bread = new Item({
+                    name: "Bread",
+                    description: "Bread toast.",
+                    price: 0.00
+                });
 
-        let egg = new Item({
-            name: "Egg",
-            description: "Fried egg.",
-            price: 1.50
-        });
+                let egg = new Item({
+                    name: "Egg",
+                    description: "Fried egg.",
+                    price: 1.50
+                });
 
-        let bacon = new Item({
-            name: "Bacon",
-            description: "Fried bacon.",
-            price: 1.50
-        });
+                let bacon = new Item({
+                    name: "Bacon",
+                    description: "Fried bacon.",
+                    price: 1.50
+                });
 
-        let cheese = new Item({
-            name: "Cheese",
-            description: "Roasted cheese.",
-            price: 1.50
-        });
+                let cheese = new Item({
+                    name: "Cheese",
+                    description: "Roasted cheese.",
+                    price: 1.50
+                });
 
-        bread.save().then(function () {
-            console.log("Added the bread item.");
-        });
+                bread.save().then(function () {
+                    console.log("Added the bread item.");
 
-        egg.save().then(function () {
-            console.log("Added the egg item.");
-        });
+                    egg.save().then(function () {
+                        console.log("Added the egg item.");
 
-        bacon.save().then(function () {
-            console.log("Added the bacon item.");
-        });
+                        bacon.save().then(function () {
+                            console.log("Added the bacon item.");
 
-        cheese.save().then(function () {
-            console.log("Added the cheese item.");
-        });
+                            cheese.save().then(function () {
+                                console.log("Added the cheese item.");
 
-        let sandwich = new Product({
-            name: "Sandwich",
-            description: "Bread toast, fried egg, fried bacon and roasted cheese.",
-            image: null,
-            price: 5.00,
-            items: [
-                bread._id,
-                egg._id,
-                bacon._id,
-                cheese._id
-            ]
-        });
+                                let sandwich = new Product({
+                                    name: "Sandwich",
+                                    description: "Bread toast, fried egg, fried bacon and roasted cheese.",
+                                    image: "http://lorempixel.com/400/400/food/",
+                                    price: 5.00,
+                                    items: [
+                                        bread._id,
+                                        egg._id,
+                                        bacon._id,
+                                        cheese._id
+                                    ]
+                                });
 
-        let refrigerant = new Product({
-            name: "Refrigerant",
-            description: "Refrigerant 350 ml.",
-            image: null,
-            price: 2.20,
-            items: null
-        });
+                                sandwich.save().then(function () {
+                                    console.log("Added the sandwich product.");
+                                });
+                            });
+                        });
+                    });
+                });
 
-        sandwich.save().then(function () {
-            console.log("Added the sandwich product.");
-        });
+                let refrigerant = new Product({
+                    name: "Refrigerant",
+                    description: "Refrigerant 350 ml.",
+                    image: "http://lorempixel.com/400/400/food/",
+                    price: 2.20,
+                    items: null
+                });
 
-        refrigerant.save().then(function () {
-            console.log("Added the refrigerant product.");
-        });
+                refrigerant.save().then(function () {
+                    console.log("Added the refrigerant product.");
+                });
 
-        res.status(201).end();
+                let frenchFries = new Product({
+                    name: "French Fries",
+                    description: "Salted French Fries.",
+                    image: "http://lorempixel.com/400/400/food/",
+                    price: 4.20,
+                    items: null
+                });
+
+                frenchFries.save().then(function () {
+                    console.log("Added the french fries product.");
+                });
+
+                res.status(201).end();
+            });
+        });
     };
 
     return controller;
