ff25566dff96d67374e435fade7f6ba8d30a50e8
Matheus Filipe T. Chaves <matheus.02468@gmail.com>
2017-09-19 09:35:29 -0300

    Adjusted the authentication controller.

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,5 +1,6 @@
 module.exports = (app) => {
     let controller = app.controllers.authentication;
+    let logger = app.utils.logger.access;
 
     app.route("/authentication")
         .post(logger, controller.authenticate);
